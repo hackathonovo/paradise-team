2481669263f12d4f067733a611c9e999eefb4a4d
BlazJurisic <blaz.jurisic@cobeisfresh.com>
2017-05-21 01:00:24 +0200

    Added chat

--- iOS-AppTemplate/AppDelegate.swift
+++ iOS-AppTemplate/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         rootCoordinator = MainCoordinator()
         _ = rootCoordinator?.start()
+        FIRApp.configure()
         return true
     }
 
